c6d464bd7e3904fb934d576dcc5b7910ae31593d
Vladislav <balenkov53@gmail.com>
2019-04-13 15:26:54 +0300

    Updated

--- lesson 8/script.js
+++ lesson 8/script.js
@@ -1,16 +1,51 @@
+let money, time;
 
-let money = +prompt("Ваш бюджет в месяц ?");
-let time = prompt("Введите дату в формате YYYY-MM-DD");
-let ascSpend = prompt("Введите обязательную статью расходов в этом месяце");
-let moneySpend = +prompt("Во сколько обойдется ?");
+function start(){
+    money = +prompt("Ваш бюджет в месяц ?");
+    time = prompt("Введите дату в формате YYYY-MM-DD");
 
-let appData = {};
+    while(isNaN(money) || money == "" || money == null){
+        money = +prompt("Ваш бюджет в месяц ?");
+    }
+}
+start();
 
-appData.money = money;
-appData.timeData = time;
-appData.expenses = {ascSpend: moneySpend};
-appData.optionalExpenses = {};
-appData.income = [];
-appData.savings = false;
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: true
+};
 
-alert("Ваш бюджет на 1 день: " + (appData.money - appData.expenses.ascSpend) / 30);
+function chooseExtenses()
+{    for(let i = 0; i < 2; i++){
+        let a = prompt("Введите обязательную статью расходов в этом месяце");
+        let b = +prompt("Во сколько обойдется ?");
+
+        if(typeof a === 'string' && typeof a != null && typeof b != null &&
+        a != '' && b != '' && a.length < 50){
+        appData.expenses[a] = b;
+        }
+        else{
+            i -= 1;
+        }
+    }
+}
+chooseExtenses();
+
+appData.moneyPerDay = (appData.budget / 30).toFixed();
+alert("Ваш бюджет на 1 день: " + appData.moneyPerDay);
+
+function checkSavings(){
+    if(appData.savings == true){
+        let save = +prompt("Какова сумма накоплений");
+        let p = +prompt("Под какой процент");
+
+        appData.monthIncome = (save/100/12 * p).toFixed(2);
+        alert("Доход в месяц с вашего дипозита:" + appData.monthIncome);
+    }
+}
+
+checkSavings();
---
+++ lesson 8/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+appData.moneyPerDay = (appData.money / 30).toFixed();
+alert("Ваш бюджет на 1 день: " + appData.moneyPerDay);
