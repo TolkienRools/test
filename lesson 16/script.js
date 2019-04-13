adc00181e75d5b658274b269d7252571dc6d9aa2
Vladislav <balenkov53@gmail.com>
2019-04-14 01:39:55 +0300

    Updated

--- lesson 16/script.js
+++ lesson 16/script.js
@@ -1,4 +1,17 @@
 
-function hello(){
-    console.log("Hello");
-}
+let button = document.querySelectorAll('button');
+
+let wrapper = document.querySelector('.wrapper');
+let div = document.createElement('div');
+
+
+div.classList.add('black');
+
+div.textContent = '<h1></h1>';
+
+wrapper.replaceChild(div,button[3]);
+
+
+
+
+
