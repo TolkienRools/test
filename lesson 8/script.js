67f7c43444e39bae4aa40fcb118ef1f2aff56049
Vladislav <balenkov53@gmail.com>
2019-04-13 13:31:40 +0300

    Fourth

--- lesson 8/script.js
+++ lesson 8/script.js
@@ -1,6 +1,16 @@
-'use strict()';
 
+let money = +prompt("Ваш бюджет в месяц ?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+let ascSpend = prompt("Введите обязательную статью расходов в этом месяце");
+let moneySpend = +prompt("Во сколько обойдется ?");
 
+let appData = {};
 
-let dd = 2;
-console.log( +'77' + 3);
+appData.money = money;
+appData.timeData = time;
+appData.expenses = {ascSpend: moneySpend};
+appData.optionalExpenses = {};
+appData.income = [];
+appData.savings = false;
+
+alert("Ваш бюджет на 1 день: " + (appData.money - appData.expenses.ascSpend) / 30);
